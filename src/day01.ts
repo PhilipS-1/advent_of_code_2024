ef3175b6edeb2164be2e23fe6a2db135558a9adc
Philip Sandmann <p.sandmann@setlog.com>
2024-12-02 16:30:28 +0100

    day 1

--- src/day01.ts
+++ src/day01.ts
@@ -1,8 +1,36 @@
 import * as fs from 'fs';
 
 const input = fs.readFileSync('input/day01.txt', 'utf-8');
-solve(input);
+console.log(solve1(input));
+console.log(solve2(input));
 
-export function solve(input: string) {
-    return 42;
+export function solve1(input: string) {
+    let numbers1: number[] = [];
+    let numbers2: number[] = [];
+    input.split('\n').forEach((line) => {
+        const pair = line.split('   ');
+        numbers1.push(Number(pair[0]));
+        numbers2.push(Number(pair[1]));
+    });
+    numbers1.sort();
+    numbers2.sort();
+    return numbers1
+        .map((number, index) => Math.abs(number - numbers2[index]))
+        .reduce((prev, next) => prev + next);
+}
+
+export function solve2(input: string) {
+    let numbers1: number[] = [];
+    let numbers2: number[] = [];
+    input.split('\n').forEach((line) => {
+        const pair = line.split('   ');
+        numbers1.push(Number(pair[0]));
+        numbers2.push(Number(pair[1]));
+    });
+    numbers1.sort();
+    numbers2.sort();
+    return numbers1
+        .map((number, index) => number * numbers2
+            .filter(number2 => number === number2).length)
+        .reduce((prev, next) => prev + next);
 }
--- tests/day01.test.ts
+++ tests/day01.test.ts
@@ -1,10 +1,16 @@
-import { solve } from '../src/day01';
+import {solve1, solve2} from '../src/day01';
 import * as fs from 'fs';
 
 describe('Day 1', () => {
-    it('should solve with the test input', () => {
+    it('should solve part 1', () => {
         const testInput = fs.readFileSync('input/day01.test.txt', 'utf-8');
-        const result = solve(testInput);
-        expect(result).toBe(42); // Replace 42 with the expected output
+        const result = solve1(testInput);
+        expect(result).toBe(11); // Replace 42 with the expected output
+    });
+
+    it('should solve part 2', () => {
+        const testInput = fs.readFileSync('input/day01.test.txt', 'utf-8');
+        const result = solve2(testInput);
+        expect(result).toBe(31);
     });
 });
