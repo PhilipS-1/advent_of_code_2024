d7520c8a1c1d4c507377a365b35425f157d5e30b
Philip <philipsandmann@gmail.com>
2024-12-07 17:50:36 +0100

    new deno setup

---
+++ src/day00.ts
@@ -0,0 +1,25 @@
+const day = Deno.mainModule.split('/').pop()?.split('.')[0];
+const input = Deno.readTextFileSync(`input/${day}.txt`);
+
+console.log(solve1(input));
+console.log(solve2(input));
+
+function solve1(input: string){
+    return null;
+}
+
+function solve2(input: string){
+    return null;
+}
+
+import { assertEquals } from "jsr:@std/assert";
+
+Deno.test("part 1", () => {
+    const testInput = Deno.readTextFileSync(`input/${day}.test.txt`);
+    assertEquals(solve1(testInput), 143);
+})
+
+Deno.test("part 2", () => {
+    const testInput = Deno.readTextFileSync(`input/${day}.test.txt`);
+    assertEquals(solve2(testInput), 123);
+})
